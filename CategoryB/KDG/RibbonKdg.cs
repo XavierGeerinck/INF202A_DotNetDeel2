b3a2297e45bcc8f19f7151b5798e9303a1d89a79
Xavier Geerinck <thebillkidy@hotmail.com>
2013-01-29 12:48:28 +0100

    WE GOT AN ADD-IN

---
+++ CategoryB/KDG/RibbonKdg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace KDG
+{
+    public partial class RibbonKdg
+    {
+        private void RibbonKdg_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
