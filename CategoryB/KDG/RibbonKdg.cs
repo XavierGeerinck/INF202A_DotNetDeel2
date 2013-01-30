879e58fc279aa45ca1050917ab963c7206eedfdd
Joey Comhaire <joey.comhaire@gmail.com>
2013-01-30 17:47:30 +0100

    eerste testje

--- CategoryB/KDG/RibbonKdg.cs
+++ CategoryB/KDG/RibbonKdg.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using Microsoft.Office.Tools.Ribbon;
 using Excel = Microsoft.Office.Interop.Excel;
-
 namespace KDG
 {
     public partial class RibbonKdg
     {
-      private Excel.Application applicationObject;
         private void RibbonKdg_Load(object sender, RibbonUIEventArgs e)
         {
-          applicationObject = new Excel.Application();
         }
 
         private void RIBNrowFiller_Click(object sender, RibbonControlEventArgs e)
         {
-          Globals.ThisAddIn.Application.ActiveCell.Value2 = "hallo";
+          Excel.Range r = (Excel.Range)Globals.ThisAddIn.Application.ActiveCell;
+          for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
+              Excel.Range cell = (Excel.Range)r[i, j];
+              cell.Interior.Color = 36;
+            }
+          }
         }
     }
 }
