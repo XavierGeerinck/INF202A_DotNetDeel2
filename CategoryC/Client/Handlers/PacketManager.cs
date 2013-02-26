aeb705a446fa3c22565046989562c16f921a644c
Xavier Geerinck <thebillkidy@hotmail.com>
2013-02-26 10:06:10 +0100

    Added stuff.

---
+++ CategoryC/Client/Handlers/PacketManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using Shared.Opcodes;
+using Client.Opcodes;
+using System.Windows.Forms;
+
+namespace Client.Handlers
+{
+    public class PacketManager
+    {
+        static Dictionary<ClientMessage, HandlePacket> OpcodeHandlers = new Dictionary<ClientMessage, HandlePacket>();
+        delegate void HandlePacket(byte[] data);
+
+        public static void DefineOpcodeHandlers()
+        {
+            Assembly currentAsm = Assembly.GetExecutingAssembly();
+            foreach (var type in currentAsm.GetTypes())
+            {
+                foreach (MethodInfo methodInfo in type.GetMethods())
+                {
+                    foreach (OpcodeAttribute attr in methodInfo.GetCustomAttributes(typeof(OpcodeAttribute), false))
+                    {
+                        if (attr != null)
+                            OpcodeHandlers[attr.Opcode] = (HandlePacket)Delegate.CreateDelegate(typeof(HandlePacket), methodInfo);
+                    }
+                }
+            }
+        }
+
+        public static bool InvokeHandler(ClientMessage opcode, byte[] data)
+        {
+            if (OpcodeHandlers.ContainsKey(opcode))
+            {
+                OpcodeHandlers[opcode].Invoke(data);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ CategoryC/Shared/PacketHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using Shared.Opcodes;
+
+namespace Shared
+{
+    public class PacketHandler
+    {
+        public static void SendPacket(TcpClient client, ClientMessage opcode, byte[] message)
+        {
+            // create the packet that will be send
+            byte[] appendedOpcode = new byte[message.Length + 1];
+            message.CopyTo(appendedOpcode, 1);
+            appendedOpcode[0] = (byte)opcode;
+            message = appendedOpcode;
+
+            // SEND
+            NetworkStream clientStream = client.GetStream();
+            clientStream.Write(message, 0, message.Count());
+            clientStream.Flush();
+        }
+    }
+}
